ded6f22aac9979241c0f069a431689ff16ab8609
teinhonglo <teinhonglo@gmail.com>
2021-02-06 12:51:17 +0800

    [Script] simpllified procedure

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ dataset="CNNDM"
 data_dir="datasets/cnndm"
 exp_dir="exp/cnndm"
 gpu=0
+n_epochs=20
 stage=1
 
 set -euo pipefail
@@ -13,22 +14,13 @@ if [ $stage -le 0 ]; then
 fi
 
 if [ $stage -le 1 ]; then
-    python train.py --cuda --gpu $gpu --data_dir $data_dir --n_epochs 1 \
+    python train.py --cuda --gpu $gpu --data_dir $data_dir --n_epochs $n_epochs \
                 --cache_dir cache/$dataset --embedding_path glove/glove.42B.300d.txt \
                 --model HSG --save_root $exp_dir --log_root $exp_dir/log \
                 --lr_descent --grad_clip -m 3 || exit 0;
 fi
-
 
 if [ $stage -le 2 ]; then
-    python train.py --cuda --gpu $gpu --data_dir $data_dir \
-                --cache_dir cache/$dataset --embedding_path glove/glove.42B.300d.txt \
-                --model HSG --save_root $exp_dir --log_root $exp_dir/log \
-                --lr_descent --grad_clip -m 3 || exit 0;
-fi
-
-
-if [ $stage -le 3 ]; then
     python evaluation.py --cuda --gpu $gpu --data_dir $data_dir \
                          --cache_dir cache/$dataset \
                          --embedding_path glove/glove.42B.300d.txt \
